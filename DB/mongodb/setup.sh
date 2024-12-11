59d3ceab182598f916ba7ff5ee9467d90ec746a4
yang-ingyu <yangik11@naver.com>
2024-12-11 14:25:48 +0900

    fix:replicaSet.js, setup.sh

--- DB/mongodb/setup.sh
+++ DB/mongodb/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-sleep 10 | echo Sleeping
+echo "Sleeping for 10 seconds to allow MongoDB to initialize..."
+sleep 10  # MongoDB가 준비될 시간을 대기
 
-mongo mongodb://mongo1:27017 replicaSet.js
+echo "Running Replica Set initialization..."
+mongosh mongodb://mongo1:27017 --file /usr/src/configs/replicaSet.js
