333a46cee0ead3718f4904047d7593ddd29692d0
yang-ingyu <yangik11@naver.com>
2024-12-17 19:21:02 +0900

    fix: async run

--- main.py
+++ main.py
@@ -1,55 +1,38 @@
-from pymongo import MongoClient
-from datetime import datetime
-from dw_crawler.darknetARMY_crawler import scrape_darknetarmy_posts
-from dw_crawler.ctifeeds_crawler import run as run_ctifeeds
-from dw_crawler.blacksuit_crawler import run as run_blacksuit
-from dw_crawler.blackbasta_crawler import run as run_blackbasta
-from dw_crawler.daixin_crawler import run as run_daixin
-from dw_crawler.darkleak_crawler import run as run_darkleak
-from dw_crawler.abyss_crawler import run as run_abyss
-from osint_crawler.tuts4you_crawler import run as run_tuts4you
-# from osint_crawler.github_crawler import run as run_github
-from osint_crawler.x00org_crawler import run as run_0x00org
+import asyncio
+from motor.motor_asyncio import AsyncIOMotorClient
+from dw_crawler.htdark_crawler import htdark
+from dw_crawler.island_crawler import island
+from dw_crawler.leakbase_crawler import leakbase
+from dw_crawler.lockbit_crawler import lockbit
+from dw_crawler.play_crawler import play
+from dw_crawler.rhysida_crawler import rhysida
+from osint_crawler.github_crawler import github
 
+async def setup_database(db_name, collection_names):
 
-def setup_database(db_name, collection_names):
-    """
-    MongoDB 데이터베이스 및 컬렉션 생성
-    """
-    print(f"[INFO] MongoDB 설정 시작: {db_name}")
-    client = MongoClient("mongodb://mongo1:30001,mongo2:30002,mongo:30003/?replicaSet=my-rs")  # MongoDB 연결
-    db = client[db_name]  # 데이터베이스 이름
+    client = AsyncIOMotorClient("mongodb://mongo1:30001,mongo2:30002,mongo:30003/?replicaSet=my-rs")
+    db = client[db_name]
 
-    # 컬렉션 생성
+    existing_collections = await db.list_collection_names()
     for collection in collection_names:
-        if collection not in db.list_collection_names():
-            db.create_collection(collection)
-            print(f"[INFO] {collection} 컬렉션 생성 완료! ({db_name})")
+        if collection not in existing_collections:
+            await db.create_collection(collection)
 
-    print(f"[INFO] MongoDB 설정 완료: {db_name}")
     return db
 
-def run_crawling():
-    db1_collections = ["abyss", "blackbasta", "blacksuit", "breachdetector", "ctifeeds",
-                       "daixin", "darkleak", "darknetARMY", "everest", "island",
-                       "leakbase", "lockbit", "play", "rhysida", "htdark"]
-    db1 = setup_database("darkweb", db1_collections)
-    db2_collections = ["github", "tuts4you", "0x00org"]
-    db2 = setup_database("osint", db2_collections)
+async def exec_crawler():
+    db_name = 'darkweb'
+    collection_name = ['htdark', 'island', 'leakbase', 'lockbit', 'play', 'rhysida', 'github']
+    db = await setup_database(db_name, collection_name)
 
-    # 첫 번째 DB에서 크롤러 실행
-    # scrape_htdark_posts(db1, pages=10)
-    scrape_darknetarmy_posts(db1, pages=3)
-    run_blacksuit(db1)
-    run_blackbasta(db1)
-    run_ctifeeds(db1)
-    run_daixin(db1)
-    run_darkleak(db1)
-    run_abyss(db1)
+    await asyncio.gather(
+        htdark(db),
+        island(db),
+        leakbase(db),
+        lockbit(db),
+        play(db),
+        rhysida(db),
+        github(db)
+    )
 
-    
-    run_tuts4you(db2)
-    # run_github(db2)
-    run_0x00org(db2)
-
-
+asyncio.run(exec_crawler())
