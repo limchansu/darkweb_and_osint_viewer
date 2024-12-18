965b9869912e2f8e0302f8b749f0e82aad0a8e59
yang-ingyu <yangik11@naver.com>
2024-12-18 17:34:47 +0900

    docker-compose.yml

---
+++ init_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sleep 10
+mongosh --host mongo1 --port 30001 --eval '
+  rs.initiate({
+    _id: "my-rs",
+    members: [
+      { _id: 0, host: "mongo1:30001" },
+      { _id: 1, host: "mongo2:30002" },
+      { _id: 2, host: "mongo3:30003" }
+    ]
+  });
+'
