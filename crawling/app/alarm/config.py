356aba2334d6f50130316a8044a272a94ebc86c3
InGyu <yangik11@naver.com>
2024-12-19 21:01:48 +0900

    Delete crawling/app/alarm/config.py

--- crawling/app/alarm/config.py
+++
@@ -1,8 +0,0 @@
-DISCORDTOKEN = ''
-
-DARKWEB_CHANNEL_ID = 1319206629878792216  # 다크웹 채널 아이디
-OSINT_CHANNEL_ID = 1319206922074984468 # 오신트 채널 아이디
-
-# 지메일만 가능
-EMAIL_SENDER = "tastyTiramisu110@gmail.com"
-EMAIL_PASSWORD = "duca bjnc ynsf mmup"
